675cbfa50c57bcf14b5df3eb166caaeca21b7764
Shaheer Shaikh <shayana803@gmail.com>
2021-03-09 01:01:44 +0500

    links updated

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'GooseFinance'
+            screenName: 'FinanceNerv'
           }}
           options={{
             height: '300',
